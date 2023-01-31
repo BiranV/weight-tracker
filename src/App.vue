73713771941486c5406afacd38cac7cc669758fc
biran varon <biraniv@gmail.com>
2023-01-31 04:31:04 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import { ref, shallowRef, computed, watch, nextTick, onMounted } from "vue";
 import Chart from "chart.js/auto";
 
 const weights = ref([]);
@@ -79,18 +79,16 @@ watch(
     </form>
 
     <div v-if="weights && weights.length > 0">
-      <h2>Last 7 days</h2>
+      <div class="title">Last 7 days</div>
       <div class="canvas-box">
         <canvas ref="canvas"></canvas>
       </div>
-      <div class="weight-history">
-        <h2>Weight History</h2>
+      <div class="weight-list">
+        <div class="title">Weight History</div>
         <ul>
           <li v-for="weight in weights">
-            <span >{{ weight.weight }} kg </span>
-            <small>
-               ({{ new Date(weight.date).toLocaleDateString() }})
-            </small>
+            <span>{{ weight.weight }} kg </span>
+            <small> ({{ new Date(weight.date).toLocaleDateString() }}) </small>
           </li>
         </ul>
       </div>
@@ -101,43 +99,68 @@ watch(
 <style>
 * {
   font-family: "montserrat", sans-serif;
-  background-color: #eee;
+  box-sizing: border-box;
 }
 
 body {
   text-align: center;
+  background-color: #eee;
 }
 
 main {
   margin: auto;
   height: 500px;
   width: 500px;
-}
-.App {
-  color: #676767;
-  text-align: center;
-  padding-top: 4rem;
+  color: #4b4b4b;
 }
 
 h1 {
   font-size: 2.5rem;
   margin-bottom: 1rem;
-  color: #4b4b4b;
 }
-h2 {
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-  color: #4b4b4b;
+
+.current {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+  text-align: center;
+  background-color: white;
+  border-radius: 50%;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  border: 4px solid #5c8cca;
+  margin: auto;
 }
+
+.current span {
+  display: block;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.current small {
+  color: #888;
+  font-style: italic;
+}
+
+div.title {
+  margin: 1rem 0 0.4rem 0;
+  text-align: left;
+}
+
 form {
   display: flex;
   justify-content: center;
   margin-top: 2rem;
 }
+
 input[type="number"] {
+  background-color: #fff;
   outline: none;
   border: none;
-  background-color: #fff;
   border-radius: 0.25rem;
   padding: 0.5rem 1rem;
   margin-right: 1rem;
@@ -145,45 +168,60 @@ input[type="number"] {
   width: 150px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
 }
+
 input[type="submit"] {
+  color: #fff;
+  background-color: #2f75d1;
   outline: none;
   border: none;
   cursor: pointer;
-  color: #fff;
   font-weight: bold;
-  text-transform: uppercase;
-
   padding: 0.5rem 1rem;
   border-radius: 0.25rem;
-  background-color: #2f75d1;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-
   opacity: 0.7;
   transition: 0.3s;
 }
+
 input[type="submit"]:hover {
   opacity: 1;
 }
 
-.canvas-box{
+.canvas-box {
   margin-bottom: 2rem;
 }
 
-ul {
-  margin:auto;
-  list-style: none;
-    overflow: auto;
-    height: 200px;
-    width: 250px;
-    padding: 0;
-   
+canvas {
+  background-color: #fff;
+  border-radius: 0.5rem;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  padding: 0.5rem;
 }
 
-li {
-  line-height: 1.5;
+.weight-list ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.weight-list ul li {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+  cursor: pointer;
 }
 
+.weight-list ul li:nth-child(even) {
+  background-color: #dfdfdf;
+}
 
+.weight-list ul li:hover {
+  background-color: #f8f8f8;
+}
+
+.weight-list ul li span {
+  font-size: 16px;
+  font-weight: bold;
+}
 </style>
